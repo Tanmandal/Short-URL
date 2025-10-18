4764ab0575dfb1b1a7339089243a4422c39d0021
Tanmay Mandal <93572359+Tanmandal@users.noreply.github.com>
2025-10-19 03:25:10 +0530

    JWT

---
+++ auth_utils.py
@@ -0,0 +1,32 @@
+import os
+from passlib.context import CryptContext
+from jose import JWTError, jwt
+from datetime import datetime, timedelta
+
+SECRET_KEY= os.getenv("SECRET_KEY")
+ALGORITHM= 'HS256'
+TOKEN_EXPIRE=30
+
+pwd_context = CryptContext(schemes=["pbkdf2_sha256"], deprecated="auto")
+
+def hash_password(password: str):
+    if len(password)==0:
+        return ""
+    return pwd_context.hash(password)
+
+def verify_password(plain_password: str, hashed_password: str):
+    return pwd_context.verify(plain_password,hashed_password)
+
+def create_access_token(data: dict,expire_delta: timedelta = None):
+    to_encode=data.copy()
+    expire = datetime.utcnow()+(expire_delta if expire_delta else timedelta(minutes=TOKEN_EXPIRE))
+    to_encode.update({"exp":expire})
+    encoded_jwt=jwt.encode(to_encode, SECRET_KEY,algorithm=ALGORITHM)
+    return encoded_jwt
+
+def decode_access_token(token:str):
+    try:
+        payload=jwt.decode(token,SECRET_KEY,algorithms=[ALGORITHM])
+        return
+    except JWTError:
+        return None
--- main.py
+++ main.py
@@ -6,12 +6,19 @@ from pymongo.errors import DuplicateKeyError
 from contextlib import asynccontextmanager
 import os
 import helper
+from auth_utils import hash_password , create_access_token ,verify_password
+from datetime import datetime
 
 MONGO_URI = os.getenv("MONGO_URI")
 
 class URLEntry(BaseModel):
     url_code: str
+    url_pass: str
     url: str
+
+class LoginEntry(BaseModel):
+    url_code: str
+    url_pass: str
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -33,9 +40,11 @@ async def create_url(entry: URLEntry):
     if not helper.validCode(entry.url_code):
         raise HTTPException(status_code=400, detail="Invalid URL code")
     entry.url=helper.formatURL(entry.url)
+    entry.url_pass=hash_password(entry.url_pass)
+    #entry.created_at=datetime.utcnow()
     try:
         await app.URLMap.insert_one(entry.model_dump())
-        return {"message": "URL created", "data": entry.model_dump()}
+        return {"message": "URL created", "data": entry.url}
     except DuplicateKeyError:
         raise HTTPException(status_code=409, detail="URL code already exists")
 
@@ -51,3 +60,14 @@ async def redirect_to_url(url_code: str):
     if entry:
         return RedirectResponse(url=entry["url"]+url_sub)
     raise HTTPException(status_code=404, detail="Short URL not found")
+
+@app.post("/login")
+async def login(login_entry:LoginEntry):
+    entry = await app.URLMap.find_one({"url_code": login_entry.url_code})
+    if entry and verify_password(login_entry.url_pass,entry.get("url_pass")):
+        access_token = create_access_token(data={"url_code": entry["url_code"]})
+        return {"access_token":access_token, "token_type":"bearer"}
+    elif entry and len(entry.url_pass)==0:
+        raise HTTPException(status_code=400, detail="Invalid request")
+    else:
+        raise HTTPException(status_code=401, detail="Invalid credentials")
