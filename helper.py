d6b62e298b8e1171a8a8ae9b8d56936be3f7984d
Tanmay Mandal <93572359+Tanmandal@users.noreply.github.com>
2025-10-18 16:07:27 +0530

    URL Format

--- helper.py
+++ helper.py
@@ -2,3 +2,9 @@ import re
 def validCode(url_code: str) -> bool:
     pattern = r'^[A-Za-z0-9_-]{3,20}$'
     return bool(re.match(pattern, url_code))
+
+def formatURL(url: str):
+    url=url.strip()
+    if not url.startswith(("http://", "https://")):
+        url="https://"+url
+    return url
